7aaff6ece2cdbdb90ae98114e4016de659a3b881
Lee yonghyun <124564947+yonghyun95@users.noreply.github.com>
2023-02-25 04:30:35 +0900

    [level 0] Title: 배열의 평균값, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -2,10 +2,11 @@ class Solution {
     public double solution(int[] numbers) {
         double answer = 0;
         
-        for(int i = 0; i<numbers.length;i++){
-            answer+= numbers[i];
-        }
+         for (int i = 0; i<numbers.length;i++){ // 갯수
+             answer += numbers[i];
+         }
+         answer = answer / numbers.length; // numbers.length - 갯수를 뜻한다.
         
-        return answer / numbers.length;
+        return answer;
     }
 }
