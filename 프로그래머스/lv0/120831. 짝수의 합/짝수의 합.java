72e1af635c8e154ed8b782f405f74feefeb5cd74
Lee yonghyun <124564947+yonghyun95@users.noreply.github.com>
2023-02-25 03:29:21 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -2,10 +2,9 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for (int i = 1; i<=n; i++){
-            if(i % 2 == 0){
-                answer += i;
-            }
+        for (int i = 2;i<=n;i= i+2){
+            if (i % 2 == 0)
+            answer = answer + i;
         }
         
         return answer;
